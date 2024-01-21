63b4412a04715f591e541146fa40a16317b355a8
madhead <siarhei.krukau@gmail.com>
2024-01-21 02:07:42 +0100

    Fix #2: GNOME 45 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,12 @@ tasks {
     listOf("42", "45").forEach { version ->
         register<Copy>(version) {
             from(version)
-            from("common")
 
             into(layout.buildDirectory.dir(version))
         }
 
         register<Zip>("extension-$version") {
             from(version)
-            from("common")
 
             archiveBaseName = "shyriiwook@madhead.me"
             destinationDirectory = layout.buildDirectory.dir("dist/$version")
